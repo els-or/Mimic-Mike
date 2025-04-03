f7d49c55b2b5749e0f4e4c4fe45dee21297f02db
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-04-02 19:17:12 -0700

    rewrote models.

---
+++ server/src/models/GameSession.ts
@@ -0,0 +1,31 @@
+import { Schema, model, Document } from 'mongoose';
+import User from './User';
+
+interface IGameSession extends Document {
+  player: typeof User;
+  score: number; 
+}
+
+// Define the schema for the Thought document
+const GameSessionSchema = new Schema<IGameSession>(
+  {
+    player: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    score: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { getters: true },
+    toObject: { getters: true },
+  }
+);
+
+const GameSession = model<IGameSession>('GameSession', GameSessionSchema);
+
+export default GameSession;
--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,4 +1,4 @@
-import Thought from './Thought.js';
+import Thought from './GameSession.js';
 import User from './User.js';
 
 export { Thought, User };

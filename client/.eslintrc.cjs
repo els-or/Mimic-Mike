8a59f80a3cd0f2431951c70b897aab7f8af3e31f
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-04-03 19:21:13 -0700

    updated package locks. I will need to update the ESLint file as well.

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  settings: { react: { version: '19.1.0' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',

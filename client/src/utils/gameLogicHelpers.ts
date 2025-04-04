ec29a81fac1a27c6b4e87db47a863856791358e0
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-04-04 16:51:56 -0700

    created a file to hold game logic helper functions. Added function to generate a random number within this file.

---
+++ client/src/utils/gameLogicHelpers.ts
@@ -0,0 +1,8 @@
+
+//TODO: update this to return in a single line once testing is done
+export const getRandomInt = (min: number, max: number): number => {
+    //return Math.floor(Math.random() * (max - min + 1)) + min;
+    const randomInt =  Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(`Random Int: ${randomInt}`);
+    return randomInt;
+}

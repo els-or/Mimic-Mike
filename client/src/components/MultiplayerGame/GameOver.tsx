0ca929e9e1ba84b150d2797baa7832412015be3d
Rachel Sutton <raesak@gmail.com>
2025-04-09 22:08:32 -0700

    updated game over screen

--- client/src/components/MultiplayerGame/GameOver.tsx
+++ client/src/components/MultiplayerGame/GameOver.tsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import "../../styles/Multiplayer.css";
 
-export function GameOver() {
+export function GameOver(props: { victory: boolean }) {
   return (
     <div className="game-over-container">
       <h1>Game Over</h1>
+      <p>{props.victory ? "You have won!" : "You have lost!"}</p>
       <p>Thanks for playing!</p>
       <div className="button-container">
         <Link to="/" className="game-button">
